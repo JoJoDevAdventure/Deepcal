715a1fbc590245c2989e58f54b455e7f46228e5e
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-04-01 07:46:44 +0100

    fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Contact from './pages/Contact';
 import Home from './pages/Home';
 
 function App() {
   
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+
+    <Home />
+
   );
 }
 
