57e433af93baa74ed0f07e9c508925c34a32aeb3
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-06-22 02:10:38 +0100

    + Blog fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       'https://api.hubapi.com/': { // Matches any request starting with /api
         target: 'https://api.hubapi.com/', // Target URL for proxied requests
-        changeOrigin: false, // Change origin header to match development server
+        changeOrigin: true, // Change origin header to match development server
         secure: false, // Allow insecure connections for development (not recommended for production)
       },
     },
