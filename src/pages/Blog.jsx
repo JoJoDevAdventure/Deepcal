4e04e2d51e4b491227ac7be80819504f77208f5a
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-06-22 01:53:40 +0100

    + Blog fix

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { fetchBlogPosts } from "../api/hubspot";
 import { Footer, NavBar } from "../components";
+import axios from "axios";
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
+
+  const fetchBlogPosts = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.hubapi.com/cms/v3/blogs/posts`,
+        {
+          headers: {
+            'authorization': `Bearer ${HUBSPOT_ACCESS_TOKEN}`,
+          },
+        }
+      );
+      console.log(response.data)
+      return response.data.results;
+    } catch (error) {
+      console.error("Error fetching blog posts:", error);
+      throw error;
+    }
+  };
 
   useEffect(() => {
     const getPosts = async () => {
