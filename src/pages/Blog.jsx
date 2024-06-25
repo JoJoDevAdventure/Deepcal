42be8c69233e295c23096c763f0ba51b277e265c
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-06-25 20:41:49 +0100

    update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,13 +5,14 @@ import axios from "axios";
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchBlogPosts = async () => {
     try {
       const response = await axios.get(
         `https://deepcal-api-2cc87306f059.herokuapp.com/api/blog-posts`
       );
-      console.log(response.data);
+      console.log('Fetched blog posts:', response.data);
       return response.data;
     } catch (error) {
       console.error("Error fetching blog posts:", error);
@@ -21,8 +22,15 @@ const Blog = () => {
 
   useEffect(() => {
     const getPosts = async () => {
-      const blogPosts = await fetchBlogPosts();
-      setPosts(blogPosts);
+      setLoading(true);
+      try {
+        const blogPosts = await fetchBlogPosts();
+        setPosts(blogPosts);
+      } catch (error) {
+        console.error("Error setting blog posts:", error);
+      } finally {
+        setLoading(false);
+      }
     };
     getPosts();
   }, []);
@@ -32,34 +40,38 @@ const Blog = () => {
       <NavBar />
       <div className="flex flex-col justify-top items-center h-auto w-full mt-[5%] mb-[5%]">
         <h1 className="text-white text-4xl mb-4">Our blogs</h1>
-        {posts && posts.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-h-[600px]">
-            {posts.map((post) => (
-              <div
-                key={post.name}
-                className="border border-white rounded-lg overflow-hidden shadow-lg mt-10 lg:mt-20 mb-10 lg:mb-20 bg-transparent hover:bg-white hover:bg-opacity-10 transition duration-300"
-              >
-                <img
-                  src={post.featuredImage}
-                  alt=""
-                  className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                  <h2 className="text-lg font-bold mb-2 text-white min-h-[100px]">{post.name}</h2>
-                  <Link
-                    to={`/blog/${post.id}`} // assuming post.id is unique for each post
-                    state={{ post }} // pass the post data as state
-                  >
-                    <button className="bg-white text-black border border-black px-4 py-2 rounded hover:bg-black hover:text-white transition duration-300">
-                      Read More
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
+        {loading ? (
           <p className="text-white">Loading posts...</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-h-[600px]">
+            {posts.length > 0 ? (
+              posts.map((post) => (
+                <div
+                  key={post.id}
+                  className="border border-white rounded-lg overflow-hidden shadow-lg mt-10 lg:mt-20 mb-10 lg:mb-20 bg-transparent hover:bg-white hover:bg-opacity-10 transition duration-300"
+                >
+                  <img
+                    src={post.featuredImage}
+                    alt={post.name}
+                    className="w-full h-48 object-cover"
+                  />
+                  <div className="p-4">
+                    <h2 className="text-lg font-bold mb-2 text-white min-h-[100px]">{post.name}</h2>
+                    <Link
+                      to={`/blog/${post.id}`}
+                      state={{ post }}
+                    >
+                      <button className="bg-white text-black border border-black px-4 py-2 rounded hover:bg-black hover:text-white transition duration-300">
+                        Read More
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="text-white">No posts available</p>
+            )}
+          </div>
         )}
       </div>
       <Footer bg={"black"}/>
