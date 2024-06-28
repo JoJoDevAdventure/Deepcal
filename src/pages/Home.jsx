eb6cea8b922dda2e6580f03f4a6fc549274d09ef
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-06-28 18:59:49 +0100

    SEO v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,12 @@ import { About, Contact, Footer, Header, Solutions } from "../components";
 const Home = () => {
   return (
     <div className="app transition-all ease-in h-full relative">
+      <Helmet>
+        <title>Deepcal</title>
+        <meta name="description" content={"DeepCal provides a software-only solution that fully automates the entire calibration process chain, speeding up development, production, and maintenance."} />
+        <meta name="keywords" content={"Sensors, Deepcal, Software calibration, sensors software, sensors calibration"} />
+        <meta property="og:title" content={"Deepcal"} />
+      </Helmet>
       <Header/>
       <About />
       <Solutions/>
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import { Footer, NavBar } from "../components";
 
 const Post = () => {
@@ -12,7 +13,17 @@ const Post = () => {
 
   return (
     <div className="bg-[#0C0C0C] px-2 md:px-14 lg:px-[12%] min-h-[100vh]">
-      <NavBar />
+      <Helmet>
+        <title>{post.name} | Deepcal Blog</title>
+        <meta name="description" content={post.excerpt || "Read this amazing blog post on Your Blog Name"} />
+        <meta name="keywords" content={post.metaDescription} />
+        <meta property="og:title" content={post.name} />
+        <meta property="og:description" content={post.metaDescription || "Read this amazing blog post on Deepcal"} />
+        <meta property="og:image" content={post.featuredImage} />
+        <meta property="og:url" content={window.location.href} />
+        <meta name="twitter:card" content="summary_large_image"/>
+      </Helmet>
+      <NavBar/>
       <div className="flex flex-col justify-top items-center h-auto w-full mt-[5%] mb-[5%] px-4 md:px-[20%]">
         <h1 className="text-white text-2xl md:text-4xl mb-4">{post.name}</h1>
         <img
