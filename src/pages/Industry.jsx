243896713933eb105ec49fc57301960f6171f572
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-08-06 18:58:40 +0100

    News letter fix + Industry news [Working]

--- src/pages/Industry.jsx
+++ src/pages/Industry.jsx
@@ -1,28 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Footer, NavBar } from "../components";
 
 const industryBlogs = [
   {
     id: 1,
-    title: "Industry Blog 1",
-    description: "A brief description of the first industry blog post.",
-    image: "link_to_image_1.jpg",
-    link: "https://example.com/blog1"
-  },
-  {
-    id: 2,
-    title: "Industry Blog 2",
-    description: "A brief description of the second industry blog post.",
-    image: "link_to_image_2.jpg",
-    link: "https://example.com/blog2"
-  },
-  {
-    id: 3,
-    title: "Industry Blog 3",
-    description: "A brief description of the third industry blog post.",
-    image: "link_to_image_3.jpg",
-    link: "https://example.com/blog3"
+    title: "Tesla car that killed motorcyclist was in 'Full Self-Driving mode'",
+    description: "A Tesla Model S car was in 'Full Self-Driving' mode (FSD) when it was involved in a fatal crash in Seattle, Washington in April this year, Washington State Police have confirmed.",
+    image: "https://www.just-auto.com/wp-content/uploads/sites/30/2024/08/Tesla2-1024x683.jpg",
+    link: "https://www.just-auto.com/newsletters/tesla-car-that-killed-motorcyclist-was-in-full-self-driving-mode"
   },
   // Add more blogs as needed
 ];
@@ -36,9 +22,9 @@ const Industry = () => {
         <div className="w-full">
           {industryBlogs.length > 0 ? (
             industryBlogs.map((blog) => (
-              <a
+              <NavLink
                 key={blog.id}
-                href={blog.link}
+                to={blog.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block border border-white rounded-lg overflow-hidden shadow-lg mb-6 bg-transparent hover:bg-white hover:bg-opacity-10 transition duration-300"
@@ -52,16 +38,12 @@ const Industry = () => {
                   <div className="p-4 md:w-2/3">
                     <h2 className="text-lg font-bold mb-2 text-white">{blog.title}</h2>
                     <p className="text-white mb-4">{blog.description}</p>
-                    <Link
-                      to={{ pathname: blog.link }}
-                      target="_blank"
-                      className="inline-block bg-white text-black border border-black px-4 py-2 rounded hover:bg-black hover:text-white transition duration-300"
-                    >
+                    <div className="inline-block bg-white text-black border border-black px-4 py-2 rounded hover:bg-black hover:text-white transition duration-300">
                       Read More
-                    </Link>
+                    </div>
                   </div>
                 </div>
-              </a>
+              </NavLink>
             ))
           ) : (
             <p className="text-white">No industry blogs available</p>
