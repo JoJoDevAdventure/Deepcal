7b643ff5268f1e5b005f4fba489d52e460c2a221
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-10-10 19:39:58 +0100

    update newsletter

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -4,6 +4,7 @@ import ContactPage from './pages/ContactPage';
 import Demo from './pages/Demo';
 import Home from './pages/Home';
 import Industry from './pages/Industry';
+import Newsletter from './pages/Newsletter';
 import Post from './pages/Post';
 
 export const Routing = () => {
@@ -16,6 +17,7 @@ export const Routing = () => {
                 <Route path="/:postid" element={<Post/>}/>
                 <Route path='industry-news' element={<Industry/>} />
                 <Route path='/demo' element={<Demo/>} />
+                <Route path='/newsletter' element={<Newsletter/>}/>
             </Routes>
         </HashRouter>
     )
